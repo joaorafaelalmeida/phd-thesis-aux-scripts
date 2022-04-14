59c4ca7c102dab3e5aa4d9776935b7f12804ee62
João Almeida <joao.rafael.almeida@ua.pt>
2022-04-14 16:01:54 +0100

    Script to validate refs

--- verify-refs.py
+++ verify-refs.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import configparser
+from pprint import pprint
 
 def help(show=False):
 	parser = argparse.ArgumentParser(description="")
@@ -19,10 +20,58 @@ def readSettings(settingsFile):
 		raise Exception("The settings file was not found!")
 	return configuration._sections
 
+def checkDuplicatedRefs(bibFile):
+	stats = {
+		"articles":0,
+		"inproceedings":0,
+		"others":0,
+		"duplicated":[],
+		"missing_doi":[],
+		"titles_without_quotes":[]
+	}
+	titles = []
+	containsDoi=False
+	currentTitle=""
+	file = open(bibFile, 'r')
+	for rline in file.readlines():
+		NEW=True
+		line = rline.lstrip()
+		if "@article" in line:
+			stats["articles"] += 1
+		elif "@inproceedings" in line:
+			stats["inproceedings"] += 1
+		elif line.startswith("@"):
+			stats["others"] += 1
+		else:
+			NEW=False
+
+		if NEW:
+			containsDoi=False
+
+		if line.startswith("title="):
+			currentTitle = line
+			title = line.replace(" ", "")
+			title = title.replace("\"", "")
+			if title in titles:
+				stats["duplicated"].append(line)
+			titles.append(title)
+			if "title=\"{" not in line:
+				stats["titles_without_quotes"].append(line)
+
+		if "doi=" in line:
+			containsDoi=True
+		
+		if line.startswith("}") and not containsDoi:
+			stats["missing_doi"].append(currentTitle)
+
+	pprint(stats, width=200)
+
+
 def main():
 	args = help()
 	settings = readSettings(args.settings)
-	print(settings)
+	bibFile = settings["bib"]["file"]
+	checkDuplicatedRefs(bibFile)
 
 if __name__ == '__main__':
 	if "/" in sys.argv[0]:
